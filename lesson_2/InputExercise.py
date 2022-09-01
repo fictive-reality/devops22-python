3e1bdfd60d462c801b60aacb44e3bd93c9af5e45
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2022-09-01 15:03:24 +0200

    First commit Lesson 2

---
+++ lesson_2/InputExercise.py
@@ -0,0 +1,15 @@
+#String input and print
+stringInput = input()
+print({stringInput})
+
+#Integer input multiply by 2 and print
+integerInput = input()
+print(f'{int(integerInput)*2}')
+
+#Dubble string input prints twice
+dubbleStringInput = input()
+print(dubbleStringInput*2)
+
+#Float input divided by 3.5 and print
+inputFloat = input()
+print(f'{float(inputFloat)/3.5}')
---
+++ lesson_2/PrintingExercise.py
@@ -0,0 +1,17 @@
+#print integer
+print(1)
+
+#print float
+print(1.2)
+
+#print ftring
+print("string")
+
+#print boolean
+print(True)
+
+#print nonetype
+print(0)
+
+#print f-string
+print(f' 1 1.2 "string" True 0')
