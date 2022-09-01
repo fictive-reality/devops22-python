bb2951193d0e03c0f53db2a863eec58441f1d382
Jonathan Friberg <jionte@gmail.com>
2022-09-01 15:09:36 +0200

    Lesson 2 hand in

---
+++ lesson_2/jonkspanda/lesson2_input.py
@@ -0,0 +1,18 @@
+#Print string
+name = input("What's your name?? ")
+print(name)
+
+#Print int doubled
+number = input("What's your favorite number?" )
+number = int(number)
+print(number * 2)
+
+#Print string twice
+word = input("Pick a random word: ")
+print(word + word)
+
+#Print float divided by 3.5
+float_number = input("Pick a random number with decimals: ")
+float_number = float(float_number)
+divided_by = 3.5
+print(float_number / divided_by)
