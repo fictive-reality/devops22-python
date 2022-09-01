e57d4c98cfdbdd0e65f87d92735a599c57700ca3
Marc Phalen <marc.phalen@hotmail.com>
2022-09-01 22:04:40 +0200

    added input and printing exercise

---
+++ lesson_2/input_exercise.py
@@ -0,0 +1,12 @@
+
+
+bil = input("Vad har du för bil? ")
+print(f"Jaha! {bil} har jag hört har hög kvalitet?")
+miltal = int (input("Hur många mil har den gått? "))
+print(f"En lågmilare! {bil} går minst {miltal *2} innan de behöver servas.")
+
+testord = input("Knacka på dörren (skriv knack)... ")
+print(f"{testord * 2} vem där? ")
+torkare = float(input("Jag hörde att man kunde få ca 80% rabatt på vindrutetorkare, vad betalade du för dina? "))
+print(f"Det rabatterade priset bör landa på {torkare // 3.5} ")
+
---
+++ lesson_2/printing_exercise.py
@@ -0,0 +1,12 @@
+heltal = 3
+decimaltal = 2.5
+slumptext = "En liten text."
+versus = (10 > 9)
+ingenting = None
+
+print(heltal)
+print(decimaltal)
+print(slumptext)
+print(versus)
+print(ingenting)
+print(f"{heltal}, {decimaltal}, {slumptext}, {versus}, {ingenting}")
