b429873770629cba3b67b4c18e1c6ba3e3028c23
Wikipedia11 <raekon92@gmail.com>
2022-09-01 18:21:53 +0200

    Kodstädning

--- lesson_2/Wikipedia11/print_program.py
+++ lesson_2/Wikipedia11/print_program.py
@@ -1,18 +1,13 @@
-a = 123
-b = 123.456
-c = "Detta är en string"
-d = True
-e = None
-# f =
-
+a = 123 # integer
+b = 123.456 # float
+c = "Detta är en string" # string
+d = True # boolean
+e = None # NoneValue
 
 print(a)
 print(b)
 print(c)
 print(d)
 print(e)
-# print(type(f))
-
-
 
 print(f"{a}, {b}, {c}, {d}, {e}, ")
