a2467cffc0e8dea0ead3970929a764762955934a
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2022-09-02 06:17:11 +0200

    minor cleanup

--- lesson_2/ExtraUppgift15.py
+++ lesson_2/ExtraUppgift15.py
@@ -1,2 +1,3 @@
+#Assigning x y z and printing out their values
 x = y = z = 10
 print(x, y, z)
--- lesson_2/InputExercise.py
+++ lesson_2/InputExercise.py
@@ -1,9 +1,9 @@
 #String input and print
-stringInput = input()
+stringInput = input("Input a string: ")
 print({stringInput})
 
 #Integer input multiply by 2 and print
-integerInput = input("Input ")
+integerInput = input("Input an integer: ")
 print(f'{int(integerInput)*2}')
 
 #Dubble string input prints twice
