79651a485a32b31a2047428a6881ff684797f690
Anton Ottosson <anton_ottosson@hotmail.com>
2022-09-02 13:13:09 +0200

    flyttat kod filer

---
+++ lesson_3/lektion3.py
@@ -0,0 +1,12 @@
+X = 1
+Y = 4
+addresses = {"Adam": "Ormvägen 5",
+"Bella": "Klockgatan 1",
+"Cornelia": "Vikingagatan 3"}
+cars = ["Volvo", "Opel", "BMW"]
+numbers1 = {1, 2, 3, X, 6}
+numbers2 = {Y, 2, 3, 4, 7}
+#print (f' addresses ["Bella"]:{addresses["Bella"]}')
+addresses ["Daniel"] = "Prinsgränd 2"
+#print(f' {len(addresses)}')
+print ((addresses.keys()[-1]))
