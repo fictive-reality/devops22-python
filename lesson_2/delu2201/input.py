487d76408cd2c4fbf6a69b19788834b900e3dae4
delu2201 <dennis.lunnelid@gmail.com>
2022-09-03 15:25:10 +0200

    lagt till lesson2 uppgift

--- lesson_2/delu2201/input.py
+++ lesson_2/delu2201/input.py
@@ -3,19 +3,19 @@ import math
 from platform import java_ver
 
 #Write out input from user
-#input_string = input("Skriv någonting: ")
+input_string = input("Skriv någonting: ")
 
-#print(input_string)
+print(input_string)
 
 #Input number from user and double it.
-#input_number = input("Skriv en siffra: ")
-#conv_number = int(input_number)
-#print(conv_number * 2)
+input_number = input("Skriv en siffra: ")
+conv_number = int(input_number)
+print(conv_number * 2)
 
 #Input string and write it out twice
-#hello_string = input('Skriv ordet "hello":')
+hello_string = input('Skriv ordet "hello":')
 
-#print(hello_string * 2)
+print(hello_string * 2)
 
 #input a float and divide by 3,5
 
@@ -24,4 +24,4 @@ my_float = input("Skriv in ett flyttal: ")
 conv_float = float(my_float)
 sum = conv_float / 3.5
 
-print(f'Ditt flyttal delat med 3.5 är:' + str(sum))
+print(f'Ditt flyttal delat med 3.5 är: ' + str(sum))
