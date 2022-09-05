27c2a2eeaa501ffb3513df63a8d6a6170e324f49
Emmanuel <frankmukasa2@gmail.com>
2022-09-05 13:34:01 +0200

    lesson_2

---
+++ lesson_2/uppgift2.py
@@ -0,0 +1,28 @@
+### `Exercise` Input
+
+#Input a string and assign it to a variable, then print the variable
+
+username = input("Enter username:")
+print("username is" + username)
+
+name = input("Enter name")
+print(f'Your name is {name}') 
+
+# Input a number and assign it to a variable, print the value doubled
+
+int_valve = input("Please enter your age:")
+print(f'your age is:{int(int_valve) *2}')
+
+int_value =int(input("write integer value:"))
+print(int_value *2)
+
+int_value =int(input("write integer value:"))
+print(f'your number is {int(int_value) *2}')
+
+text = input("text:")
+print(text *20)
+
+#Input a float and assign it to a variable, print the value divided by 3.5
+
+num = float(input("Write a flaot number:"))
+print(num / 3.5)
