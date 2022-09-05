73b7ea04cace9ca2d73d52d241cc0ab1f4b495ea
Alex Roman <alexandru.roman@yh.nackademin.se>
2022-09-05 13:20:00 +0200

    Done with exercise 4

--- lesson_4/AlexRoman/exercise_4_5.py
+++ lesson_4/AlexRoman/exercise_4_5.py
@@ -7,3 +7,11 @@
 #   4. If the text equals stop, break the while loop                            #
 #   5. If the text don't equals stop, print the text and the length of the text #
 #################################################################################
+
+
+while True:
+    text = input("Enter a word: ").capitalize()
+    if(text == "Stop"):
+        break
+    print("Enter STOP to quit!")
+    print(f"The text you input is {text} and the lenght is {len(text)}")
