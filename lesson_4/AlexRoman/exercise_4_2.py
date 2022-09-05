23954fb255ced8f1e0cb18e9f6baa2ea855ed2e7
Alex Roman <alexandru.roman@yh.nackademin.se>
2022-09-05 11:11:24 +0200

    Done with 1 and 2

--- lesson_4/AlexRoman/exercise_4_2.py
+++ lesson_4/AlexRoman/exercise_4_2.py
@@ -6,3 +6,12 @@
 #    3. If the user input is in the tuple, print the text "Welcome {name} your are on the list".    #
 #    4. If the user input is not in the tuple, print "Sorry, you are not on the list".              #
 #####################################################################################################
+
+
+user_name = input("Enter your name: ").capitalize()
+vip_list = ("Adam", "Eve", "John", "Michael", "Anders")
+
+if(user_name in vip_list):
+    print(f"Welcome {user_name}, you are on the list!")
+else:
+    print(f"Sorry, you are not on the list!")
