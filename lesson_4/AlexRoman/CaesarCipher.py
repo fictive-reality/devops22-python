22c0f12448d41ec7de3d1b27e3c6e0729529f27e
Alex Roman <alexandru.roman@yh.nackademin.se>
2022-09-05 16:23:08 +0200

    Done with extraextra, CaesarCipher

--- lesson_4/AlexRoman/CaesarCipher.py
+++ lesson_4/AlexRoman/CaesarCipher.py
@@ -1,15 +1,14 @@
 # Caesar Cipher
 
-user_text = input("Enter the text you want encrypted : ").upper()
+from curses.ascii import islower
+
+# User input
+user_text = input("Enter the text you want encrypted : ")
 user_shift = int(input("Enter the ammount of sidesteps: "))
-
-
-
-#shift = 3 # the shift count
-#text = "HELLO WORLD"
+# Initialize empty variable
 encryption = ""
 for c in user_text:
-    #chech if character is an uppercase letter
+    #check if character is an uppercase letter
     if c.isupper():
         # find the position in 0 - 25
         c_unicode = ord(c)
@@ -21,8 +20,20 @@ for c in user_text:
         new_character = chr(new_unicode)
         # append to encrypted string
         encryption = encryption + new_character
+    # check if character is a lowercase letter
+    elif c.islower():
+        # find the position in 0 - 25
+        c_unicode = ord(c)
+        c_index = ord(c) - ord("a")
+        # perform the shift
+        new_index = (c_index + user_shift) % 26
+        # convert to new character
+        new_unicode = new_index + ord("a")
+        new_character = chr(new_unicode)
+        # append to encrypted string
+        encryption = encryption + new_character  
     else:
-        # since character is not uppercase, leave it as it is
+        # since character is not a letter, leave it as it is
         encryption += c
 print("Plaint text: ",user_text)
 print("Encrypted text: ", encryption)
