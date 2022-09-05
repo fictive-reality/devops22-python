2a3874685acca74aa30e6128ef5ab6b0ad529077
aminalawer <akmffp2713@gmail.com>
2022-09-05 11:43:11 +0200

    lesson_2, Exercise_Printing

---
+++ lesson_2/Exercise_Printing.py
@@ -0,0 +1,22 @@
+#Print a integer value
+my_int = 4
+print(my_int)
+
+#Print a float value
+my_float = 4.0
+print(my_float)
+
+#Print a String
+my_string = "hello word"
+print(my_string)
+
+#Print a boolean value
+my_bool = True
+print(my_bool)
+
+#Print a None value
+my_nothing = None
+print(my_nothing)
+
+#Print a f-string containing all previous values in one line i.e 1, 1.0, "hello"
+print(f'{my_int}, {my_float}, {my_string}, {my_bool}, {my_nothing}')
