69058cfd7d5ad971e365c26ec36561f7b603aa3a
Wikipedia11 <raekon92@gmail.com>
2022-09-05 11:43:14 +0200

    Exercise 3 finished

--- lesson_4/Wikipedia11/Extra_Exercise_PDF.py
+++ lesson_4/Wikipedia11/Extra_Exercise_PDF.py
@@ -1,21 +1,46 @@
 # -----------------------------Exercise 1-----------------------------
 
-word = "Hello"
+# Write a program that greets the user 10 times
 
-word_len = len(word)
+word = "Hello"
 
 for i in range(10):
     print(word)
 
 # -----------------------------Exercise 2-----------------------------
 
-
-
+# Write a program with for-loop that prints the following:
+#
+# 1
+# 22
+# 333
+# 4444
+# 55555
+# 666666
+# 7777777
+# 88888888
+# 999999999
 
 # -----------------------------Exercise 3-----------------------------
 
+# while True:
+    # word = input("Please enter a word. To stop, enter 'stop': ")
+    # if word == "stop":
+        # break
+    # else:
+    #   len_word = len(word)
+    #   print(f"The word is {word}, which has {len_word} characters")
+    #   print("Enter 'stop' to quit")
 
-
+while True:
+    user_num = int(input("Enter a integer: "))
+    if user_num == 74:
+        print("Correct")
+        break
+    elif user_num < 74:
+        print("Wrong, it's higher")
+    elif user_num > 74:
+        print("Wrong, it's lower.")    
 
 # -----------------------------Exercise 4-----------------------------
 
