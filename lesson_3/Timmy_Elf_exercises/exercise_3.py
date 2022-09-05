ee992211d501288a63176472516ec1437620fdd6
Timmy Elf <Timmy.Elf@th.nackademin.se>
2022-09-05 15:28:13 +0200

    Added exercises for lesson 3.

---
+++ lesson_3/Timmy_Elf_exercises/exercise_3.py
@@ -0,0 +1,23 @@
+#Boolean Operations - and, or, not
+
+x = True
+y = False
+z = True
+
+#i.
+print(x or y or z)
+
+#ii.
+print(x and y or z)
+
+#iii.
+print(x and y and z)
+
+#iv.
+print((not x and z) or y)
+
+#v.
+print(not (x or y or z))
+
+#vi.
+print(not (x and z) or y)
