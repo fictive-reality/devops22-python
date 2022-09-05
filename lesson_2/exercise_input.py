f0e7b504e7fcdd9cbd6798166080d340b37c6cbc
Raffi Avakian <raffi.avakian@yh.nackademin.se>
2022-09-05 12:15:31 +0200

    Lesson 2 Excercise 2 complete

---
+++ lesson_2/exercise_input.py
@@ -0,0 +1,22 @@
+#Create a program that uses input and type conversion
+# 1. Input a string and assign it to a variable, then print the variable
+# 2. Input a number and assign it to a variable, print the value doubled
+# 3. Input a string i.e hello and assign it to a variable, print the string repeated hellohello
+# 4. Input a float and assign it to a variable, print the value divided by 3.5
+
+#1
+question_1 = input("what is your name? ")
+print(question_1)
+
+#2
+number = int(input("Write any number to double "))
+print(number * 2)
+
+#3
+hello = input("Write hello ")
+print(hello * 2)
+
+#4
+floaty = float(input("Write any number "))
+print(floaty /3.5)
+
