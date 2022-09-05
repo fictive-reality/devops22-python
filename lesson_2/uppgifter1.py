d4ad864c657a381fc1658aecbc5391845af50fc3
CharalamposMoutsios <Charalampos.Moutsios@yh.nackademin.se>
2022-09-05 12:40:16 +0200

    lesson_2

---
+++ lesson_2/uppgifter1.py
@@ -0,0 +1,48 @@
+
+
+#Uppgifter 1 Printing
+
+# 1. Print a integer value
+
+num = 3
+print (num)
+
+# 2. Print a float value
+
+fnum = 4.0
+print (fnum)
+
+# 3. Print a string
+
+name = ("Charalampos")
+print (name)
+
+# 4. Print a boolean value
+
+a = 200
+b = 30
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+  
+# 5. Print a None value
+
+value = None
+print (value)
+
+
+# 6. Print a f-string containing all previous values in one line i.e 1, 1.0, "hello" ..
+
+print(f'  {num} , {fnum} , {name} , "b is not greater than a" , {value} ')
+
+
+  
+  
+  
+
+    
+    
+    
+    
---
+++ lesson_2/uppgifter2.py
@@ -0,0 +1,23 @@
+#uppgifter 2 Input
+
+#Create a program that uses `input` and type conversion
+
+# 1. Input a string and assign it to a variable, then print the variable
+
+name = input("Please enter your name: ")
+print(f' Your name is {name}')
+
+# 2. Input a number and assign it to a variable, print the value doubled
+
+age = input("Please enter your age: ")
+print (f' Your age doubled is: {int(age)* 2}')
+
+# 3. Input a string i.e `hello` and assi
+
+text = input("Write a text: ")
+print (text * 5)
+
+# 4. Input a float and assign it to a variable, print the value divided by 3.5'
+
+a_float = float(input("write a float number devided by 3.5: "))
+print(a_float / 3.5)
