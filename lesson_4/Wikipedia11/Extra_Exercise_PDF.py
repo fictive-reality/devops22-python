8cd7828b657a287ef9241fe7684dad8f089bd178
Wikipedia11 <raekon92@gmail.com>
2022-09-05 13:04:25 +0200

    Exercise 4 finished

--- lesson_4/Wikipedia11/Extra_Exercise_PDF.py
+++ lesson_4/Wikipedia11/Extra_Exercise_PDF.py
@@ -1,12 +1,14 @@
-# -----------------------------Exercise 1-----------------------------
+import random
 
+# -----------------------------Exercise 1-----------------------------
+""""
 # Write a program that greets the user 10 times
 
 word = "Hello"
 
 for i in range(10):
     print(word)
-
+"""
 # -----------------------------Exercise 2-----------------------------
 
 # Write a program with for-loop that prints the following:
@@ -23,7 +25,7 @@ for i in range(10):
 
 # -----------------------------Exercise 3-----------------------------
 
-# while True:
+"""# while True:
     # word = input("Please enter a word. To stop, enter 'stop': ")
     # if word == "stop":
         # break
@@ -41,11 +43,27 @@ while True:
         print("Wrong, it's higher")
     elif user_num > 74:
         print("Wrong, it's lower.")    
-
+"""
 # -----------------------------Exercise 4-----------------------------
 
+# Write a program that loops over a list containing different numbers. If the program crashes
+# on an uneven number write the words “Not allowed!” out and the loop is terminated.
 
+# Working as intended.
+# i found the line "random_index = random.randint(0,len(num_list)-1)" searching on google but i do understand what it does.
+# It goes into a module and pulls out a random int which im using as a index for my list
 
+num_list = [1, 6, 8, 9, 45, 44, 75, 23, 76, 60, 78, 23, 70]
+random_index = random.randint(0,len(num_list)-1)
+
+while True:
+    num_list = [1, 6, 8, 9, 45, 44, 75, 23, 76, 60, 78, 23, 70]
+    random_index = random.randint(0,len(num_list)-1)
+    if num_list[random_index] % 2 == 0:
+        continue
+    else:
+        print("Not allowed!")
+        break
 
 # -----------------------------Exercise 5-----------------------------
 
