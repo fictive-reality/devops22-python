69665118537c3c733dbe1595e38fdacf3aaa08b9
Anton Ottosson <anton_ottosson@hotmail.com>
2022-09-05 15:07:02 +0200

    k

--- lesson_3/lektion3.py
+++ lesson_3/lektion3.py
@@ -1,20 +1,30 @@
+from copy import copy, deepcopy
+
+
 X = 1
 Y = 4
 addresses = {"Adam": "Ormvägen 5",
 "Bella": "Klockgatan 1",
 "Cornelia": "Vikingagatan 3"}
+
 cars = ["Volvo", "Opel", "BMW"]
 numbers1 = {1, 2, 3, X, 6}
 numbers2 = {Y, 2, 3, 4, 7}
 #print (f' addresses ["Bella"]:{addresses["Bella"]}')
+
 addresses ["Daniel"] = "Prinsgränd 2"
+
 #print(f'{len(addresses)}')
+
 #print (list (addresses.items())[-1][1])
+
 #addresses : addresses = {v: k for k, v in addresses.items() }
+
 #print (sorted (addresses)[0])
-#cars.sort()
+cars.sort()
 #print (cars [0])
 cars_2 = cars 
 cars.append("Saab")
-print (cars)
-print (cars_2)
+cars_3 = copy(cars)
+cars_3.pop()
+print (cars,cars_2,cars_3)
