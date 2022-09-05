704bdf7ba881cbe0de32064ebe90796136316ad7
Alex Roman <alexandru.roman@yh.nackademin.se>
2022-09-05 11:21:50 +0200

    Done with 1,2 and 3

--- lesson_4/AlexRoman/exercise_4_3.py
+++ lesson_4/AlexRoman/exercise_4_3.py
@@ -1,6 +1,13 @@
 #####################################################################################################
 #  Exercise 3 Repeat word                                                                           #
-#   1. Write a program that fulfills the specification:                                             #
-#   2. Ask the user for a word                                                                      #
-#   3. Print the word x times, where x = len(word). i.e if the word is hello the program writes:    #
+#   Write a program that fulfills the specification:                                                #
+#   1. Ask the user for a word                                                                      #
+#   2. Print the word x times, where x = len(word). i.e if the word is hello the program writes:    #
 #####################################################################################################
+
+
+word = input("Enter a word: ")
+x = len(word)
+print(f"I will write your word as many times as the amount of leters in the word")
+for i in range(x):
+    print(word)
