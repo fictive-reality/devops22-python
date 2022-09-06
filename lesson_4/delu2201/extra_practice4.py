1ec1688f759697a4299ef2d9a7f70a6128e798bc
Dennis Lunnelid <dennis.lunnelid@gmail.com>
2022-09-06 17:22:53 +0200

    extra practice klar

--- lesson_4/delu2201/extra_practice4.py
+++ lesson_4/delu2201/extra_practice4.py
@@ -45,6 +45,22 @@ for n in list1:
 else:
     print("No odd numbers found.")
 
+#Övning 5
+
+first_list = [3, 7, 9, 2, 6]
+second_list = [2, 3, 6, 7, 9]
+my_list = []
+#my_tuple = {}
+for x in range(0, len(second_list)):
+    temp_var = second_list[x]
+    temp_var_igen = first_list.index(temp_var)
+    #print(f" {temp_var} {first_list.index(temp_var)}")
+    my_tup = (temp_var, first_list.index(temp_var))
+    my_list.append(my_tup)
+#print(first_list.index(temp_var))
+#print(my_tup)
+print(my_list)
+
 
 #Övning 7
 
