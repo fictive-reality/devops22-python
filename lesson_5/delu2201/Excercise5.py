82dc7543fbcfb84679b5ddecf3abc3a3c7f71937
Dennis Lunnelid <dennis.lunnelid@gmail.com>
2022-09-07 16:35:54 +0200

    uppgift 2 klar

--- lesson_5/delu2201/Excercise5.py
+++ lesson_5/delu2201/Excercise5.py
@@ -1,3 +1,6 @@
+from audioop import reverse
+
+
 firstname = "john"
 lastname = "smith"
 tele = "00468123456789"
@@ -5,9 +8,21 @@ tele = "00468123456789"
 #1
 print(f'{firstname}, {lastname}, {tele}')
 
-fullname = firstname + " " + lastname + " " + tele
+fullname = firstname + " " + lastname
 print(fullname)
 
 print(f' {len(fullname)} {len(firstname)} {len(lastname)}')
 
 print(f"{fullname}\n{tele}")
+
+print(fullname + " " + tele)
+print(f'{fullname} {tele}')
+print('{} {}'.format(fullname,  tele))
+#print(f'Ditt namn är {} och ditt nummer är {}, fullname, tele')
+
+#2
+print(fullname[:5])
+print(fullname[1:-1])
+print(fullname.upper()[0:-1:2])
+print(fullname[::-1])
+print(fullname[5:5])
