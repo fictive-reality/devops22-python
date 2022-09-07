d8a989fdab27fb58ed4af228a1aaf8af6e23b045
Dennis Lunnelid <dennis.lunnelid@gmail.com>
2022-09-07 13:45:16 +0200

    fixade len(fullname)

---
+++ lesson_5/delu2201/Excercise5.py
@@ -0,0 +1,13 @@
+firstname = "john"
+lastname = "smith"
+tele = "00468123456789"
+
+#1
+print(f'{firstname}, {lastname}, {tele}')
+
+fullname = firstname + " " + lastname + " " + tele
+print(fullname)
+
+print(f' {len(fullname)} {len(firstname)} {len(lastname)}')
+
+print(f"{fullname}\n{tele}")
