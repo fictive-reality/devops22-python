c0e66c6c197fe8c605acb45dcc6733b5191980ad
Marc Phalen <marc.phalen@hotmail.com>
2022-09-08 18:39:09 +0200

    added uppgift_A

---
+++ lesson_3/uppgift_A.py
@@ -0,0 +1,31 @@
+X = 1
+Y = 4
+addresses = {"Adam": "Ormvägen 5",
+"Bella": "Klockgatan 1",
+"Cornelia": "Vikingagatan 3"}
+addresses["Daniel"] = "Prinsgränd 2"
+cars = ["Volvo ","Volvo", "Opel", "Opel", "BMW ", "BMW",]
+numbers1 = {1, 2, 3, X, 6}
+numbers2 = {Y, 2, 3, 4, 7}
+#addresses = {v: k for k, v in addresses.items()}
+#print(f'{list(addresses)[-1]}')
+#cars.sort(reverse=True)
+#cars_2 = cars.copy()
+#cars.append("Saab")
+#cars_3 = cars_2.copy()
+#print(cars)
+#unique_cars = set(cars)
+#print(unique_cars)
+#intersection_numbers = numbers1 & numbers2
+#print(intersection_numbers)
+#union_numbers = numbers1.union(numbers2)
+#print(union_numbers)
+
+#print("I cars finns: ", cars)
+#print("I cars_2 finns: ", cars_2)
+#print("I cars_3 finns: ", cars_3)
+
+
+#print(numbers1 - numbers2)
+
+
