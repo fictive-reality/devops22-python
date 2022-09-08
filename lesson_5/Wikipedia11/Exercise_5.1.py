1ec83ab940061139b50d0c91a3161dcdfbd318c8
Fredrik <raekon92@gmail.com>
2022-09-08 19:04:03 +0200

    Exercise 5.1 finished

--- lesson_5/Wikipedia11/Exercise_5.1.py
+++ lesson_5/Wikipedia11/Exercise_5.1.py
@@ -10,4 +10,26 @@
 first_string = input("Give me a string: ")
 second_string = input("Give me a second string: ")
 
-print(f'"{first_string}"\n"{second_string}"')
+# \n New line
+print(f'"{first_string}"\n"{second_string}"') 
+
+# \' to write single quote, \\' to write double quote
+print(f'"{first_string}"\'"{second_string}"')
+
+# To ignore escape sequence
+print(f'"{first_string}"\r"{second_string}"')
+
+# puts a tab
+print(f'"{first_string}"\t"{second_string}"')
+
+# Backspace
+print(f'"{first_string}"\b"{second_string}"')
+
+# This i dont know, its supposed to print one word 
+# then indent the next. Needs to look into this more
+print(f'"{first_string}"\f"{second_string}"')
+
+# Same with this one, look into it more
+print(f'"{first_string}"\v"{second_string}"')
+
+print(f'"{first_string}"\0"{second_string}"')
