bc58803a0842d51bbd23919a064525a36561281c
Fredrik <raekon92@gmail.com>
2022-09-08 18:35:31 +0200

    Exercise 5 finished

--- lesson_5/Wikipedia11/Exercise_4_Extra.py
+++ lesson_5/Wikipedia11/Exercise_4_Extra.py
@@ -7,3 +7,21 @@
 # 5. The boss calculates the percentage and respond yes or no, you decide which criteria the boss uses. 4 and 5 iterates in a loop until the employee quit or the boss accept the amount.
 
 # ------------------------------------EXERCISE 4------------------------------------
+
+current_sallery = 2500
+
+print(f"You current sallery is €{current_sallery}")
+emp_raise = int(input("How much of a raise are you looking for? €"))
+
+# emp_raise / current_sallery * 100 = raise in percent
+
+percent_raise = emp_raise / current_sallery * 100 
+emp_raise = int(input("\U0001f645" " Please counter give a lower suggestion: "))
+while True:
+    if percent_raise > 3:
+        emp_raise = int(input("\U0001f645" " Please counter give a lower suggestion: "))
+        percent_raise = emp_raise / current_sallery * 100 
+    else:
+        new_sallery = current_sallery + emp_raise
+        print(f"Congratulations, your new salery will be €{new_sallery}")
+        break
