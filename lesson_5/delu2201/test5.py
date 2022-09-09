12c0d2a2eb703ab3034584fe2fb4e8c7c2100a21
Dennis Lunnelid <dennis.lunnelid@gmail.com>
2022-09-09 09:03:57 +0200

    Sparar testfilen

--- lesson_5/delu2201/test5.py
+++ lesson_5/delu2201/test5.py
@@ -1,14 +1,31 @@
-firstname = "john"
-lastname = "smith"
-tele = "00468123456789"
 
-#1.
+#4
+salery = 20000
+print(f"your current salary is {salery}. How much more are you asking for?")
+annual_raise = int(input("Enter amount: "))
+new_salary = salery + annual_raise
+perc_increase = (new_salary * 100 / salery) - 100
+count = 0
 
-print(f'{firstname}, {lastname}, {tele}')
-
-fullname = firstname + " " + lastname
-print(fullname)
-
-print(f' {len(fullname)} {len(firstname)} {len(lastname)}')
-
-print(f"{fullname}\n{tele}")
+while count < 5:
+        if count == 0:
+            print(f"You asked for a {perc_increase}% increase... NO! \U0001F923")
+            annual_raise = int(input("Try again!: "))
+            new_salary = salery + annual_raise
+            perc_increase = (new_salary * 100 / salery) - 100
+            count = count + 1
+        else:
+            for i in range(4):
+                count = count + 1
+                if perc_increase <= 5:
+                    print(f"Sure, you'll get {new_salary}!")
+                    break
+                else:    
+                    print(f"You asked for a {perc_increase}% increase... FUCK NO! \U0001F923")
+                    annual_raise = int(input("Try again!: "))
+                    new_salary = salery + annual_raise
+                    perc_increase = (new_salary * 100 / salery) - 100
+            print("These negotiations are going nowhere, BYE!")
+            break
+#Jag är här för dig Dennis!! <3
+#Du lär dig dag för dag min vän! If else we trust!
