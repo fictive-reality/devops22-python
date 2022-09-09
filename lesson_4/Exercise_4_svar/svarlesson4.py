c671a6d0285bfcd4f43b81ec722340ec78d474d7
Isabel Valijani <isabel.valijani@yh.nackademin.se>
2022-09-09 16:14:31 +0200

    inlämning lesson_1

---
+++ lesson_4/Exercise_4_svar/svarlesson4.py
@@ -0,0 +1,89 @@
+#from tokenize import Number
+
+
+#Number = int(input("enter a integer:"))
+#if (Number % 2) ==0:
+   # print ("Your number is even.")
+#else: print("Your number is odd.")
+
+
+
+
+#my_name = input ("Medlemskort?:")
+#print (f"Hej")
+
+#my_tuple = ("VIP", "vanlig", "guld","silver", "brons", "svart",)
+#print (my_tuple)
+
+#if my_name in my_tuple :
+   #print ("Welcome your are on the list.")
+#else: 
+   #print ("Sorry your not on the list.")
+ #OUTPUT   
+#Medlemskort?:
+#grå
+#Hej
+#('VIP', 'vanlig', 'guld', 'silver', 'brons', 'svart')
+#Sorry your not on the list.
+
+
+#for _ in range(10):
+#from re import X
+#print("Bella")
+
+
+#X= len("Isabel")
+#print(X)
+
+#from re import X
+
+
+#for i in range(6):
+   # print(i)
+
+
+#num = 4
+#total = sum(range(1, num + 1))
+#print(total) 
+
+
+#user_input = [input('sum of all integers ') for x in range(10)] 
+
+
+
+#from tokenize import group
+#from unicodedata import name
+
+
+#a = 1
+#while a==1:
+  # b = name
+#input ("Vad heter du")
+
+#print("Hej", b, "", "Valkommen till gruppen!")
+
+
+#while True:
+    #print("Vad heter du")
+    
+
+
+prompt = "\nHej och valkommen till gruppen!"
+prompt += "\nWrite 'stop' when you're done with the question. "
+
+while True:
+     group = input (prompt)
+     
+     if group == 'stop':
+          break
+
+     else:
+          print (f"Du är med i grupp omega! {group} contains", len(group), "letters.")
+
+
+print("Tack för idag:)")
+
+
+
+
+
