f4ac4b7beb10340f9e69085f00984955fbfa8094
Alex Roman <alexandru.roman@yh.nackademin.se>
2022-09-12 15:17:35 +0200

    Update exercise_6_2.py
    found how to solve the last part

--- lesson_6/AlexRoman/exercise_6_2.py
+++ lesson_6/AlexRoman/exercise_6_2.py
@@ -10,3 +10,47 @@
 #    3. In reversed alphabetical order
 
 # Start working on 6.2
+from operator import indexOf
+import random
+from random import shuffle, choices, sample
+from typing import Counter
+
+separation = "==================================================="
+seed_list = ["johan", "lisa", "tove", "tobias", "jonas", "sara"]
+long_list = random.choices(seed_list, k = 100)
+
+print(f"The list of all the names: {long_list}")
+print(separation)
+
+count_names = {}
+for name in long_list:
+    if name in count_names:
+        count_names[name] += 1
+    else:
+        count_names[name] = 1
+        
+count = Counter(count_names)
+common = count.most_common()
+
+
+print(common)
+
+
+print(f"The name count: {count_names}")
+print(separation)
+print(f"The most popular names: {count}")
+print(separation)
+print(f"The first 3 most polular names: {common[0:3]}")
+
+print(separation)
+
+
+
+print(f"Names in alphabetical order: {common.sort()}")
+print(f"Names in shuffled order: {common}")
+print(f"Names reverse alphabetical order: {common.reverse()}")
+
+
+
+# use this
+print([tup[0] for tup in common])
