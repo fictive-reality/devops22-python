d7a23f3e7fee133459226fbe3baa33659240a5d8
Wikipedia11 <raekon92@gmail.com>
2022-09-13 15:00:56 +0200

    Exercise 1 finished

--- lesson_7/Wikipedia11/Exercise_1_Functions.py
+++ lesson_7/Wikipedia11/Exercise_1_Functions.py
@@ -10,10 +10,31 @@
 #    2. Create a function that prints a string in capital letters, with `word` as a parameter
 #    3. Create a function that prints numbers between 1 and `stop`, where stop is a parameter
 
+import string
 # ------------------------------------EXERCISE 1------------------------------------
 
+def do_nothing():
+    pass
 
 # ------------------------------------EXERCISE 2------------------------------------
 
+def hello_world():
+    print("hello world")
 
+def numbers():
+    print(1 == 1.0)
+
+def alphabet():
+    print(string.ascii_lowercase[::-1])
 # ------------------------------------EXERCISE 3------------------------------------
+
+def hello_name(name):
+    print("Hello, " + name + ". Good morning!")
+
+def upper_string(word):
+    print(f"{word.upper()} ")
+
+def num_print(stop, start=1):
+    for n in range(start, stop):
+        print(n)
+
