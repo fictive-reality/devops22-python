26f639fd5311d2ac14541dc43fd431fb31575c45
Wikipedia11 <raekon92@gmail.com>
2022-09-15 14:11:56 +0200

    initial commit

---
+++ lesson_10/Wikipedia11/Exercise_1_Override.py
@@ -0,0 +1,19 @@
+# 1. Create a class that has at least one method
+# 2. Create a subclass that uses your class (1) as base class
+# 3. In the subclass (2) you should override the method from (1)
+# 4. Print a instance of (1) and (2) both calling the same method name
+
+
+# ------------------------------------EXERCISE 1------------------------------------
+
+
+
+# ------------------------------------EXERCISE 2------------------------------------
+
+
+
+# ------------------------------------EXERCISE 3------------------------------------
+
+
+
+# ------------------------------------EXERCISE 4------------------------------------
---
+++ lesson_10/Wikipedia11/Exercise_3_Composition_VS_Inheritance.py
@@ -0,0 +1,27 @@
+# In this exercise you will write two alternative solutions, one with inheritance and one with composition.
+
+#### Inheritance
+
+# 1. Create a class named `Person`, it should have `address` stored, with street name, street number, postal code, country.
+# 2. Create a class `Employee` that uses `Person´ as base class. Add employee number and salary.
+
+#### Composition
+
+# 1. Create a class `Address` with street name, street number, postal code, country.
+# 2. Create a class `Employee` that has an address class, employee number and salary
+
+
+
+# ------------------------------------EXERCISE 1(Inheritance)------------------------------------
+
+
+
+# ------------------------------------EXERCISE 2(Inheritance)------------------------------------
+
+
+
+# ------------------------------------EXERCISE 1(Composition)------------------------------------
+
+
+
+# ------------------------------------EXERCISE 2(Composition)------------------------------------
